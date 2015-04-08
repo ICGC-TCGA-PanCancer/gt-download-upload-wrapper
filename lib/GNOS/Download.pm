2e43034ba531e151fccd9c08181c595a1d7053ef
Adam <adam.j.wright82@gmail.com>
2015-04-08 15:16:54 -0400

    removing file::tail and just using tail

--- lib/GNOS/Download.pm
+++ lib/GNOS/Download.pm
@@ -7,8 +7,6 @@ use feature qw(say);
 use autodie;
 use Carp qw( croak );
 
-use File::Tail;
-
 use constant {
     MILLISECONDS_IN_AN_HOUR => 3600000,
 };
@@ -38,8 +36,8 @@ sub run_download {
         $time_stamp = sprintf("%04d-%02d-%02d-%02d-%02d-%02d", 
                                  $now[5]+1900, $now[4]+1, $now[3],
                                  $now[2],      $now[1],   $now[0]);
- 
-        $random_int = int(rand(100000));
+
+        $random_int = int(rand(1000));
         $log_filepath = "gtdownload-$time_stamp-$random_int.log"; 
         say "STARTING DOWNLOAD WITH LOG FILE $log_filepath ATTEMPT ".++$attempt." OUT OF $max_attempts";
 
@@ -61,44 +59,43 @@ sub run_download {
     
 }
 
-
 sub read_output {
-    my ($output_log, $timeout) = @_;
+    my ($log_filepath, $timeout) = @_;
 
     my $start_time = time;
     my $time_last_downloading = 0;
     my $last_reported_percent = 0;
 
-    my $file=File::Tail->new($output_log);
-    my $line;
+    my ($line, $process);
 
-    while( defined($line=$file->read) ) {
+    while( $line = `tail -n 1 $log_filepath` ) {
+        sleep 10;
         my ($size, $percent, $rate) = $line =~ m/^Status:\s*(\d+.\d+|\d+|\s*)\s*[M|G]B\s*downloaded\s*\((\d+.\d+|\d+|\s)%\s*complete\)\s*current rate:\s+(\d+.\d+|\d+| )\s+MB\/s/g;
         $percent = $last_reported_percent unless( defined $percent);
         
         my $md5sum = ($line =~ m/^Download resumed, validating checksums for existing data/g)? 1: 0;
 
-        if ($percent == 100) {
-            say "FINISHED DOWNLOAD at TIME: time";
-            return 0;
-        }      
-        elsif ( ($percent > $last_reported_percent) || $md5sum) {
+        $process = `ps aux | grep 'gtdownload -l $log_filepath'`;
+        return 0 unless ($process =~ m/children/); # This checks to see if the gtdownload process is still running. Does not say if completed correctly       
+
+        if ( ($percent > $last_reported_percent) || $md5sum) {  # Checks to see if the download is making progress.
             $time_last_downloading = time;
             say "UPDATING LAST DOWNLOAD TIME: $time_last_downloading";
             say "  REPORTED PERCENT DOWNLOADED - LAST: $last_reported_percent CURRENT: $size" if ($percent > $last_reported_percent);
             say "  IS MD5Sum State: $md5sum" if ($md5sum);
         }
         elsif ((($time_last_downloading != 0) and ( (time - $time_last_downloading) > $timeout) )
-                 or ( ($percent == 0) and ( (time - $start_time) > (3 * $timeout)) )) { 
+                 or ( ($percent == 0) and ( (time - $start_time) > (3 * $timeout)) )) { # this check is to see the download has not been working for too long
                 # This should trigger if gtdownload stops being able to download for a certain amount of time 
                 #     or if it never starts downloading - giving time for the md5 check
             say "BASED ON OUTPUT DOWNLOAD IS NEEDING TO BE RESTARTED";
             return 1;
         }
+
         $last_reported_percent = $percent;
     }
 
-    return 0;
+    return 1;
 }
 
 1;
